35e3b289d37ade323e612a48f96a5f2ca9964f60
RafaelGP <mr.rafaelgp@gmail.com>
2015-05-12 21:22:50 +0100

    Fixed several syntax mistakes in rgp-yasnippet layer

--- rgp-yasnippet/packages.el
+++ rgp-yasnippet/packages.el
@@ -3,7 +3,9 @@
 (defun rgp-yasnippet/init-yasnippet ()
   "Initialize rgp-yasnippet"
   (use-package yasnippet
-    :mode ("\\.yasnippet\\'" . snippet-mode)
+    :mode "\\.yasnippet\\'"
+    :defer t
     :config
-    (yas-global-mode)
-    (yas-load-directory "~/.emacs.d/private/snippets/")))
+    (progn
+      (setq yas-snippet-dirs "~/.emacs.d/private/snippets/"))
+    ))
