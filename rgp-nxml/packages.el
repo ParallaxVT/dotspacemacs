2354ffdae7927816e2d0332949ce17ab543afaf0
RafaelGP <mr.rafaelgp@gmail.com>
2015-05-12 21:19:53 +0100

    Fixed several syntax mistakes in rgp-nxml layer

--- rgp-nxml/packages.el
+++ rgp-nxml/packages.el
@@ -3,14 +3,14 @@
 (defun rgp-nxml/init-nxml-mode ()
   "Initialize rgp-nxml"
   (use-package nxml-mode
-    :mode (("\\.xml\\'" . nxml-mode))
+    :mode "\\.xml\\'"
     :defer t
     :preface
-    (add-hook 'nxml-mode-hook
-              '(lambda()
-                 (setq nxml-child-indent 4)
-                 (setq indent-tabs-mode nil)))
-    (setq nxml-slash-auto-complete-flag t)          ;; Auto close tag after typing /
-    (electric-pair-mode t)                          ;; Close autematically parenthesis and double quotes
-    (electric-indent-mode t)                        ;; Indent a line after pressing return
-    ))
+    (progn
+      (add-hook 'nxml-mode-hook (lambda () (run-hooks 'prog-mode-hook))) ;; Add nxml-mode to the progn-mode-hook
+      (setq nxml-child-indent 4)
+      (setq indent-tabs-mode nil)
+      (setq nxml-slash-auto-complete-flag t)          ;; Auto close tag after typing /
+      (electric-pair-mode t)                          ;; Close autematically parenthesis and double quotes
+      (electric-indent-mode t)                        ;; Indent a line after pressing return
+      )))
