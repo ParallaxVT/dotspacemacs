64c74c3a52f2d63e9aa8cd59969380652069a6d9
RafaelGP <mr.rafaelgp@gmail.com>
2015-05-14 11:05:55 +0100

    Added some values to 'ido-ignore-files' to avoid display './' and '../'

--- rgp-ido/packages.el
+++ rgp-ido/packages.el
@@ -7,7 +7,7 @@
     :preface
     (progn
       (setq ido-file-extensions-order '(".xml" ".html" ".css" ".ps1" ".sh" ".el" ".php" ".org")) ;; Enphasize files with the specified extensions
-      (setq ido-ignore-files '("\\.jpg$" "\\.jpeg$" "\\.gif$" "\\.png$" "\\.swf$" "\\.sa$" "\\.fla$" "\\.elc"))
+      (setq ido-ignore-files '("\\.jpg$" "\\.jpeg$" "\\.gif$" "\\.png$" "\\.swf$" "\\.sa$" "\\.fla$" "\\.elc" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./"))
       (setq ido-max-prospects 50)
       (define-key ido-file-completion-map "\C-v" 'ido-exit-minibuffer)
       (define-key ido-file-completion-map "\C-w" 'ido-up-directory)
