1c58b657f25e2d6be6a7dfa8266ed42e99cb6f1a
RafaelGP <mr.rafaelgp@gmail.com>
2015-08-10 17:05:38 +0100

    Yasnippet - Expand snippet using TAB
    Removed 'yas-snipet-dirs' var as it's already set by spacemacs

--- rgp-yasnippet/packages.el
+++ rgp-yasnippet/packages.el
@@ -3,9 +3,23 @@
 (defun rgp-yasnippet/init-yasnippet ()
   "Initialize rgp-yasnippet"
   (use-package yasnippet
-    :mode "\\.yasnippet\\'"
-    :defer t
     :config
     (progn
-      (setq yas-snippet-dirs "~/.emacs.d/private/snippets/"))
+      (defun yas-advise-indent-function (function-symbol)
+        (eval `(defadvice ,function-symbol (around yas-try-expand-first activate)
+                 ,(format
+                   "Try to expand a snippet before point, then call `%s' as usual"
+                   function-symbol)
+                 (let ((yas-fallback-behavior nil))
+                   (unless (and (interactive-p)
+                                (yas-expand))
+                     ad-do-it)))))
+
+      (yas-advise-indent-function 'indent-for-tab-command)
+      (add-hook 'nxml-mode-hook
+                (let ((original-command (lookup-key nxml-mode-map [tab])))
+                  `(lambda ()
+                     (setq yas-fallback-behavior
+                           '(apply ,original-command))
+                     (local-set-key [tab] 'yas-expand)))))
     ))
