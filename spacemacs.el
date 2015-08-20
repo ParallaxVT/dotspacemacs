ba15c392d240a4844df308dd1b337200fbadf8db
Rafael <rafael@debian>
2015-08-20 15:37:00 +0100

    Keep the menu bar hidden

--- spacemacs.el
+++ spacemacs.el
@@ -117,7 +117,6 @@ layers configuration."
                 (substring (emacs-version) 0 16) "."))         ;; New scratch buffer text
   (global-prettify-symbols-mode 1)                             ;; Display a lambda symbol itstead of the word
   (setq frame-title-format '(buffer-file-name "%f" ("%b")))    ;; Show file path in the title bar if available. Buffer name otherwise
-  (menu-bar-mode 1)                                            ;; Show menu bar
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
